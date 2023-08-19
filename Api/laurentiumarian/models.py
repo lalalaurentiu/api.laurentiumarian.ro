abf020e87673d52b95266b9bde8cff62f63c4348
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-08-19 20:28:07 +0300

    create navapps and apps model and migrate them

--- Api/laurentiumarian/models.py
+++ Api/laurentiumarian/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class NavApps(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    
+class Apps(models.Model):
+    CHOICES = (
+        ('Folder', 'Folder'),
+        ('File', 'File'),
+        ('App', 'App'),
+    )
+    app = models.ForeignKey(NavApps, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices=CHOICES)
+    img = models.ImageField(upload_to='images/', null=True, blank=True)
+    def __str__(self):
+        return self.name
