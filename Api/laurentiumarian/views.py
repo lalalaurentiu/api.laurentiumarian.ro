7f9c60c647bd327b6e82b1e0299bf4fb12274a7e
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-08-19 20:29:48 +0300

    create view for data

--- Api/laurentiumarian/views.py
+++ Api/laurentiumarian/views.py
@@ -1,7 +1,33 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .serializers import *
+from .models import *
 
 class AppsView(APIView):
     def get(self, request):
-        return Response({
-            'laurentiumarian': 'hello world'})
+        queryset = Apps.objects.all()
+        serializer = CustomAppsSerializer(queryset, many=True)
+
+        data = [
+            {
+                "NavApps": []
+            }
+        ]
+
+        for app in serializer.data:
+
+            for i in range(len(data[0]["NavApps"])):
+                if data[0]["NavApps"][i]["name"] == app["app"]["name"]:
+                    data[0]["NavApps"][i]["apps"].append(app)
+                    break
+            else:
+                data[0]["NavApps"].append({
+                    "name": app["app"]["name"],
+                    "apps": [app]
+                })
+
+        return Response(
+            data,
+            status=200
+
+        )
